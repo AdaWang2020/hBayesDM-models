bfb12539e806d2edca66f45bcfff0ba55edff0b5
Jethro Lee <dlemfh96@gmail.com>
2019-02-23 10:10:34 +0900

    Update ValidateAll.sh

--- json_data/ValidateAll.sh
+++ json_data/ValidateAll.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Written by Jetho Lee
 
-for i in `ls | grep '^[a-z]'`; do
+for i in `ls | grep '\.json$' | grep '^[a-z]'`; do
   echo "========== $i =========="
   jsonschema -i "$i" ModelInformation.schema.json
 done
